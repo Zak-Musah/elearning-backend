fb056e06eba4b33e7f5e6510b0feaf95d97d5c25
Abdul-Razak <arazak4r@gmail.com>
2022-01-10 22:53:56 +0100

    Initial commit: Login/Register implemented

---
+++ models/user.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    school: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    year: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    program: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    picture: {
+      type: String,
+      default: "/avatar.png",
+    },
+    role: {
+      type: [String],
+      default: ["Student"],
+      enum: ["Student", "Instructor", "Admin"],
+    },
+    stripe_account_id: "",
+    stripe_seller: {},
+    stripeSession: {},
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
