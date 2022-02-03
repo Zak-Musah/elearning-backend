4490bf1bab18cfe7ef13ffeeae91f4c2005e2ac3
Abdul-Razak <arazak4r@gmail.com>
2022-02-03 22:35:19 +0100

    feat: added business info route

--- routes/business.js
+++ routes/business.js
@@ -6,11 +6,16 @@ const router = express.Router();
 // import { isCreator, requireSignin } from "../middlewares";
 
 // controllers
-import { createContent, uploadImage } from "../controllers/business";
+import {
+  createContent,
+  uploadImage,
+  getBusinessInfo,
+} from "../controllers/business";
 
 // image upload
 router.post("/business/upload-image", uploadImage);
 
+router.get("/business-info", getBusinessInfo);
 router.post("/business", createContent);
 
 module.exports = router;
